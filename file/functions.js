17f15b7b7ddd36b3052b67b976e693c4f54045a7
SAM <sam@SAMs-MacBook-Air.local>
2024-07-01 19:09:28 +0530

    major changes in the css

--- file/functions.js
+++ file/functions.js
@@ -37,28 +37,61 @@ $(window).resize(function() {
 
 
 
-function timeElapse(date){
+// function timeElapse(date){
+// 	var current = Date();
+// 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+// 	var days = Math.floor(seconds / (3600 * 24));
+// 	seconds = seconds % (3600 * 24);
+// 	var hours = Math.floor(seconds / 3600);
+// 	if (hours < 10) {
+// 		hours = "0" + hours;
+// 	}
+// 	seconds = seconds % 3600;
+// 	var minutes = Math.floor(seconds / 60);
+// 	if (minutes < 10) {
+// 		minutes = "0" + minutes;
+// 	}
+// 	seconds = seconds % 60;
+// 	if (seconds < 10) {
+// 		seconds = "0" + seconds;
+// 	}
+// 	var result =  "Days <span class=\"digit\">" + days + "</span> Hours <span class=\"digit\">" + hours + "</span> Minutes <span class=\"digit\">"+ minutes + "</span> Seconds <span class=\"digit\">" + seconds + "</span>";; 
+// 	$("#clock").html(result);
+
+// 	var text = "THE WORLD JUST GOT LUCKIER SINCE ";
+// 	$("#message-box").html(text);
+
+// }
+
+
+function timeElapse(date) {
 	var current = Date();
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
 	var days = Math.floor(seconds / (3600 * 24));
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
 	if (hours < 10) {
-		hours = "0" + hours;
+	  hours = "0" + hours;
 	}
 	seconds = seconds % 3600;
 	var minutes = Math.floor(seconds / 60);
 	if (minutes < 10) {
-		minutes = "0" + minutes;
+	  minutes = "0" + minutes;
 	}
 	seconds = seconds % 60;
 	if (seconds < 10) {
-		seconds = "0" + seconds;
+	  seconds = "0" + seconds;
 	}
-	var result =  "Days <span class=\"digit\">" + days + "</span> Hours <span class=\"digit\">" + hours + "</span> Minutes <span class=\"digit\">"+ minutes + "</span> Seconds <span class=\"digit\">" + seconds + "</span>";; 
+	
+	var result = "";
+	if (window.innerWidth <= 480) {
+	  result = "<span class=\"digit\">" + days + "</span>d <span class=\"digit\">" + hours + "</span>h <span class=\"digit\">"+ minutes + "</span>m <span class=\"digit\">" + seconds + "</span>s";
+	} else {
+	  result = "Days <span class=\"digit\">" + days + "</span> Hours <span class=\"digit\">" + hours + "</span> Minutes <span class=\"digit\">"+ minutes + "</span> Seconds <span class=\"digit\">" + seconds + "</span>";
+	}
+	
 	$("#clock").html(result);
-
+  
 	var text = "THE WORLD JUST GOT LUCKIER SINCE ";
 	$("#message-box").html(text);
-
-}
+  }
